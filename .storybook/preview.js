1f39cec0aff0a235d4ec08f3968c73006070f5a7
thewanionly <wani.pitogo@gmail.com>
2022-02-24 05:18:36 +0800

    Install sass, convert css file to scss. Install storybook scss preset

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '../src/styles/index.css'
+import '../src/styles/main.scss'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './styles/index.css'
 
 const App = () => {
   return <div>Test</div>

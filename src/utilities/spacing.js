1f9449e0872080f8e31ab9a93745e755a26f20ba
thewanionly <wani.pitogo@gmail.com>
2022-02-27 06:22:46 +0800

    Make `spacing` variable into a map. Define spacing constant

---
+++ src/utilities/spacing.js
@@ -0,0 +1,13 @@
+const spaces = {
+  xxxl: 'xxxl', //56px
+  xxl: 'xxl', //48px
+  xl: 'xl', //40px
+  lg: 'lg', //32px
+  md: 'md', //24px
+  base: 'base', //16px
+  sm: 'sm', //8px
+  xs: 'xs', //4px
+  none: 'none' //0px
+}
+
+export default Object.freeze(spaces)

28bf061de646b7eefb3b988533c89ea18a0f0af7
thewanionly <wani.pitogo@gmail.com>
2022-02-27 06:54:27 +0800

    Add more sizes in spacing

--- src/utilities/spacing.js
+++ src/utilities/spacing.js
@@ -1,9 +1,20 @@
 const spaces = {
-  xxxl: 'xxxl', //56px
-  xxl: 'xxl', //48px
-  xl: 'xl', //40px
-  lg: 'lg', //32px
-  md: 'md', //24px
+  'xxl-4': 'xxl-4', //208px
+  'xxl-3': 'xxl-3', //192px
+  'xxl-2': 'xxl-2', //176px
+  'xxl-1': 'xxl-1', //160px
+  'xl-4': 'xl-4', //144px
+  'xl-3': 'xl-3', //128px
+  'xl-2': 'xl-2', //112px
+  'xl-1': 'xl-1', //96px
+  'lg-4': 'lg-4', //80px
+  'lg-3': 'lg-3', //72px
+  'lg-2': 'lg-2', //64px
+  'lg-1': 'lg-1', //56px
+  'md-4': 'md-4', //48px
+  'md-3': 'md-3', //40px
+  'md-2': 'md-2', //32px
+  'md-1': 'md-1', //24px
   base: 'base', //16px
   sm: 'sm', //8px
   xs: 'xs', //4px
